d461bd528fc5e24254d507f01c3adead16c526d2
conkraw <conkraw@gmail.com>
2024-11-21 14:55:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+import streamlit as st
 
+st.set_page_config(layout="wide")
+
+# Importing the content of the pages
+from welcome import show_welcome_page
+from opd import show_opd  # Example of another page import
+
+# Initialize session state variable to handle page routing
+if "page" not in st.session_state:
+    st.session_state.page = "welcome"  # Default page is welcome
+
+def main():
+    # Page routing based on session state
+    if st.session_state.page == "welcome":
+        show_welcome_page()
+    elif st.session_state.page == "another_page":
+        show_opd()
