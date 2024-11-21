847bbd1393c606c00c461366107b918c77d885dd
conkraw <conkraw@gmail.com>
2024-11-21 14:39:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import pandas as pd
 import datetime
 
+# Initialize session state variables
+if 'date_submitted' not in st.session_state:
+    st.session_state['date_submitted'] = False
+
 # Display the title of the app
 st.title('OPD Page')
 
@@ -23,11 +27,31 @@ if page_option == 'Create OPD':
                 # Try to parse the date entered by the user
                 test_date = datetime.datetime.strptime(x, "%m/%d/%Y")
                 
+                # Store the date in session state
+                st.session_state['date_submitted'] = True
+                st.session_state['test_date'] = test_date.strftime('%m/%d/%Y')
+
                 # Display the converted date
                 st.write(f"Valid date entered: {test_date.strftime('%m/%d/%Y')}")
+                st.write("You will now be redirected to the File Upload page.")
+                st.experimental_rerun()  # Trigger the rerun to navigate to file upload
+
             except ValueError:
                 # Display an error message if the date format is incorrect
                 st.error('Invalid date format. Please enter the date in m/d/yyyy format.')
         else:
             # If no date is entered, display an error
             st.error('Please enter a date.')
+
+# If the date is submitted, show the file upload page
+if st.session_state['date_submitted']:
+    st.write('Upload the following Excel files:')
+    
+    uploaded_files = {}
+    uploaded_files['HOPE_DRIVE.xlsx'] = st.file_uploader('Upload HOPE_DRIVE.xlsx', type='xlsx')
+    uploaded_files['ETOWN.xlsx'] = st.file_uploader('Upload ETOWN.xlsx', type='xlsx')
+    uploaded_files['NYES.xlsx'] = st.file_uploader('Upload NYES.xlsx', type='xlsx')
+    
+    # You can process the uploaded files here
+    if all(uploaded_files.values()):
+        st.write("All files uploaded successfully!")
