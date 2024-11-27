9fc0f34cd856f94e8f4068dbcc3d8c9fcffdc28d
conkraw <conkraw@gmail.com>
2024-11-26 20:28:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1772,12 +1772,12 @@ elif st.session_state.page == "Create List":
         a7 = pd.DataFrame(clinictype, columns = ['type'])
 
         a1['type']=clinictype
-	a2['type']=clinictype
-	a3['type']=clinictype
-	a4['type']=clinictype
-	a5['type']=clinictype
-	a6['type']=clinictype
-	a7['type']=clinictype
+        a2['type']=clinictype
+        a3['type']=clinictype
+        a4['type']=clinictype
+        a5['type']=clinictype
+        a6['type']=clinictype
+        a7['type']=clinictype
 		
     except Exception as e:
         st.error(f"Error processing the HOPE_DRIVE sheet: {e}")
