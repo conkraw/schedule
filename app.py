a9df9cf419e5e9ac4724e4f3a78662c70da47cc9
conkraw <conkraw@gmail.com>
2025-01-29 21:46:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6013,6 +6013,20 @@ elif st.session_state.page == "Create List":
 
         df.to_excel('Source1.xlsx', index=False)
 
+	output = io.BytesIO()
+	with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+	    df.to_excel(writer, index=False, sheet_name='Sheet1')
+	    writer.close()
+	output.seek(0)
+	
+	# Download button
+	st.download_button(
+	    label="Download Excel File",
+	    data=output,
+	    file_name="Source1.xlsx",
+	    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+)
+
         import openpyxl
         import numpy as np 
 
