f9da66f675ec01d263421561098f193d8298d384
conkraw <conkraw@gmail.com>
2025-02-05 21:26:49 -0500

    Update appu.py

--- appu.py
+++ appu.py
@@ -2022,7 +2022,7 @@ elif st.session_state.page == "OPD Creator":
 	dfx.to_csv('wardp.csv', index=False)
 
 	# Replace "Rounder" values with "AM - Continuity"
-	WARDP = dfx.replace({"On-Call": "AM - Continuity","On-Call 8a-8a": "AM - Continuity"},regex=True)
+	WARDP = dfx.replace({"On-Call": "AM - Continuity","On-Call 8a-8a ": "AM - Continuity"},regex=True)
 
 	# Identify rows with "AM - Continuity"
 	am_continuity_rows = WARDP[WARDP.eq("AM - Continuity ").any(axis=1)].copy()
