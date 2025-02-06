c4077d38e1d7919024c5f55e22d59bc9bab5a6a4
conkraw <conkraw@gmail.com>
2025-02-05 20:55:57 -0500

    Update appu.py

--- appu.py
+++ appu.py
@@ -2058,16 +2058,16 @@ elif st.session_state.page == "OPD Creator":
 	        ws1.cell(row=target_row, column=target_column).value = row[5].value  # Value in column F (index 5)
 	        ws1.cell(row=target_row, column=target_column).alignment = Alignment(horizontal='center')
 	
-    # Save updated workbook
-    wb1.save('OPD.xlsx')
+    	# Save updated workbook
+    	wb1.save('OPD.xlsx')
     
-    df=pd.read_csv('final.csv',dtype=str)
-    import io
-    output = io.StringIO()
-    df.to_csv(output, index=False)
-    output.seek(0)
+    	df=pd.read_csv('final.csv',dtype=str)
+    	import io
+    	output = io.StringIO()
+    	df.to_csv(output, index=False)
+    	output.seek(0)
     
-    st.download_button(label="Download CSV File",data=output.getvalue(),file_name="final.csv",mime="text/csv")
+    	st.download_button(label="Download CSV File",data=output.getvalue(),file_name="final.csv",mime="text/csv")
 
 	import openpyxl
 	from openpyxl.styles import Alignment
