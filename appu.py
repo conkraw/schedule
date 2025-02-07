ced41cd20ec8d6350b66b4b9d291dd5f1b9724e0
conkraw <conkraw@gmail.com>
2025-02-07 09:47:50 -0500

    Update appu.py

--- appu.py
+++ appu.py
@@ -1001,7 +1001,7 @@ elif st.session_state.page == "Create List":
             print("Invalid date format in the cell.")
         else:
             # Format the date to mm-dd-yyyy
-            formatted_date = test_date.strftime('%m-%d-%Y')
+            formatted_date = test_date.strftime('%m/%d/%Y') 
 
             # Calculate the start date (use test_date directly as it's already a datetime object)
             start_date = test_date
