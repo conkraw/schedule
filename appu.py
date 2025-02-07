7c08d77ea0734d666565ccc4b56e6c76ae571cf9
conkraw <conkraw@gmail.com>
2025-02-07 10:19:04 -0500

    Update appu.py

--- appu.py
+++ appu.py
@@ -1030,10 +1030,11 @@ elif st.session_state.page == "Create List":
         # Convert 'date' column to datetime and then format it
         dateMAP['date'] = pd.to_datetime(dateMAP['date'])
         dateMAP['date'] = dateMAP['date'].dt.strftime('%m/%d/%Y')
-	    
+
         dateMAP.to_csv('xxxDATEMAP.csv', index=False)
         
-    	a1 = a2 = a3 = a4 = a5 = a6 = a7 = pd.read_excel(uploaded_opd_file, sheet_name='HOPE_DRIVE').iloc[3:23, [0]].rename(columns={0: 'type'})
+        a1 = a2 = a3 = a4 = a5 = a6 = a7 = pd.read_excel(uploaded_opd_file, sheet_name='HOPE_DRIVE').iloc[3:23, [0]].rename(columns={0: 'type'})
+	    
         #read_file = pd.read_excel(uploaded_opd_file, sheet_name='HOPE_DRIVE')
         #read_file.to_csv ('hopedrive.csv', index = False, header=False)
         #df=pd.read_csv('hopedrive.csv')
