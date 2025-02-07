cc772993bbb9df3128dca20add145896446412a7
conkraw <conkraw@gmail.com>
2025-02-06 22:30:06 -0500

    Update appu.py

--- appu.py
+++ appu.py
@@ -560,7 +560,7 @@ elif st.session_state.page == "OPD Creator":
 	
 	    # ✅ **Apply replacements if provided**
 	    if replacements:
-	        dfx = dfx.replace(replacements, regex=False)
+	        dfx = dfx.replace(replacements, regex=True)
 	
 	    # ✅ **Save the cleaned file**
 	    filename = f"{clinic_name.lower()}.csv"
