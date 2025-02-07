0fee9b4eb5ed8ccc1ed63e0aed1a04adaa20c801
conkraw <conkraw@gmail.com>
2025-02-07 09:16:40 -0500

    Update appu.py

--- appu.py
+++ appu.py
@@ -989,7 +989,7 @@ elif st.session_state.page == "Create List":
     try:
         df = pd.read_excel(uploaded_opd_file)
         st.dataframe(df)
-        test_date = df.iloc[3, 1]
+        test_date = df.iloc[2, 1]
 
         # Ensure that test_date is a valid datetime object
         # If it's a string, convert it into a datetime object using pd.to_datetime
