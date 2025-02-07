d876437a39adfd541555a674ba83b037309f5ed6
conkraw <conkraw@gmail.com>
2025-02-07 09:23:53 -0500

    Update appu.py

--- appu.py
+++ appu.py
@@ -987,7 +987,7 @@ elif st.session_state.page == "Create List":
     
     # Ensure the "HOPE_DRIVE" sheet exists in the uploaded Excel file
     try:
-        df = pd.read_excel(uploaded_opd_file)
+        df = pd.read_excel(uploaded_opd_file, skiprows=1, header=None)
         st.write("Row 2 contents:", df.iloc[2, :].tolist())
         test_date = df.iloc[2, 1]; st.write("Extracted test_date:", test_date)
 
