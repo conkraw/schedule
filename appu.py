d42c38f673f992fc7c594a460158000380b005b5
conkraw <conkraw@gmail.com>
2025-02-06 21:48:22 -0500

    Update appu.py

--- appu.py
+++ appu.py
@@ -756,6 +756,9 @@ elif st.session_state.page == "OPD Creator":
 	x = pd.read_csv('26.csv')
 	st.dataframe(x)
 
+	y = pd.read_csv('27.csv')
+	st.dataframe(y)
+
 	############################################################################################################################
 	tables = {f"t{i}": pd.read_csv(f"{i}.csv") for i in range(1, 28)}
 	t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16, t17, t18, t19, t20, t21, t22, t23, t24, t25, t26, t27 = tables.values()
