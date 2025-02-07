4d4ea6fba46f2450672d282dde8794180eaa671f
conkraw <conkraw@gmail.com>
2025-02-07 10:18:31 -0500

    Update appu.py

--- appu.py
+++ appu.py
@@ -1033,7 +1033,7 @@ elif st.session_state.page == "Create List":
 	    
         dateMAP.to_csv('xxxDATEMAP.csv', index=False)
         
-	a1 = a2 = a3 = a4 = a5 = a6 = a7 = pd.read_excel(uploaded_opd_file, sheet_name='HOPE_DRIVE').iloc[3:23, [0]].rename(columns={0: 'type'})
+    	a1 = a2 = a3 = a4 = a5 = a6 = a7 = pd.read_excel(uploaded_opd_file, sheet_name='HOPE_DRIVE').iloc[3:23, [0]].rename(columns={0: 'type'})
         #read_file = pd.read_excel(uploaded_opd_file, sheet_name='HOPE_DRIVE')
         #read_file.to_csv ('hopedrive.csv', index = False, header=False)
         #df=pd.read_csv('hopedrive.csv')
