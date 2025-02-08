f6d27bc8d0c7f6e92c76a03c46707a5860d34120
conkraw <conkraw@gmail.com>
2025-02-07 20:21:50 -0500

    Update appu.py

--- appu.py
+++ appu.py
@@ -762,6 +762,9 @@ elif st.session_state.page == "OPD Creator":
 	wardc_df = (pd.concat([wcard_df, wgi_df, wnephro_df], ignore_index=True).query("type == 'AM - Continuity '").assign(clinic="WARD_C").groupby(["date", "clinic"], as_index=False).agg({"type": "first", "provider": lambda x: "/".join(x)}))
 
 	picu_df = process_file("PICU.xlsx", "PICU", replacement_rules.get("PICU.xlsx")); st.dataframe(picu_df)
+	picu_df = picu_df[picu_df["type"] == "AM - Continuity"]["provider"].value_counts()
+	picu_df = picu_df[picu_df > 1].index.tolist()
+	picu_df = picu_df[(picu_df["type"] != "AM - Continuity") | (picu_df["provider"].isin(repeating_providers)) | (picu_df.groupby(["date", "type"])["provider"].transform("count") == 1) 
 
 	special_clinics = {"AAC","HAMPDEN_NURSERY"}
 	
