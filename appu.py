ef6cd8f783584a964643af7c822f121f66314fd1
conkraw <conkraw@gmail.com>
2025-02-12 08:14:59 -0500

    Update appu.py

--- appu.py
+++ appu.py
@@ -2183,7 +2183,7 @@ elif st.session_state.page == "Create List":
         st.write("Duplicate Check:"); st.dataframe(df_duplicates);                
 
         provider_df = (df[df['student'].notna() & (df['student'].str.strip() != "")].assign(date=pd.to_datetime(df['date'], errors='coerce')).groupby(['student', 'providers'], as_index=False)['date'].max().assign(eval_due_date=lambda x: x['date'] + pd.Timedelta(days=14)))
-        df = df.assign(Providers=df["Providers"].str.split("/")).explode("providers"); st.write("Evaluation Due Dates:");st.dataframe(provider_df); provider_df.to_csv('provider_df.csv',index=False)
+        provider_df = provider_df.assign(Providers=provider_df["providers"].str.split("/")).explode("providers"); st.write("Evaluation Due Dates:");st.dataframe(provider_df); provider_df.to_csv('provider_df.csv',index=False)
 
         provider_df = pd.read_csv("provider_df.csv")
         mapping_df = pd.read_csv("mapping_df.csv")  # The file with 'name' and 'Formatted Name'
