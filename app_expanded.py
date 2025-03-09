80b6d4b60d2cf8f7b118d50c4e93dc7ba977ff85
conkraw <conkraw@gmail.com>
2025-03-09 16:02:34 -0400

    Update app_expanded.py

--- app_expanded.py
+++ app_expanded.py
@@ -1657,12 +1657,12 @@ elif st.session_state.page == "Student Assignments":
             (21, 150)   # T21 to T27 starts at 78
         ]
 
-	combined_mapping = {}
-	for start_t, start_value in t_mappings:
-		for i in range(start_t, start_t + 7):
-		combined_mapping[f"T{i}"] = generate_mappingx(start_value)
-		
-	return combined_mapping
+        combined_mapping = {}
+        for start_t, start_value in t_mappings:
+            for i in range(start_t, start_t + 7):
+            combined_mapping[f"T{i}"] = generate_mappingx(start_value)
+
+        return combined_mapping
 	    
     def process_excel_mapping(location, sheet_name):
         """
