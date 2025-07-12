faf8392d8da0489d4c140192f11917a4f8bc81cc
conkraw <conkraw@gmail.com>
2025-07-11 20:00:27 -0400

    Update app2026.py

--- app2026.py
+++ app2026.py
@@ -836,7 +836,7 @@ elif st.session_state.page == "OPD Creator":
 	hampdennursery_df = process_file("HAMPDEN_NURSERY.xlsx", "HAMPDEN_NURSERY", replacement_rules.get("HAMPDEN_NURSERY.xlsx"))
 	sjrhosp_df = process_file("SJR_HOSP.xlsx", "SJR_HOSP", replacement_rules.get("SJR_HOSP.xlsx"))
 	aac_df = process_file("AAC.xlsx", "AAC", replacement_rules.get("AAC.xlsx"))
-	ahouloukpe_df = process_file("AL.xlsx", "AL", replacement_rules.get("AL.xlsx"))
+	al_df = process_file("AL.xlsx", "AL", replacement_rules.get("AL.xlsx"))
 	
 	adolmed_df = process_file("ADOLMED.xlsx", "ADOLMED", replacement_rules.get("ADOLMED.xlsx"))
 	adolmed_df = adolmed_df[adolmed_df["provider"] == "Shook, Jennifer"] #Only Extract Jennifer Shook
