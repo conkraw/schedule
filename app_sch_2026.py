ef49e09bd3169737f4e3b9381485409dbf921424
conkraw <conkraw@gmail.com>
2025-07-15 19:04:46 -0400

    Update app_sch_2026.py

--- app_sch_2026.py
+++ app_sch_2026.py
@@ -355,10 +355,15 @@ def generate_opd_workbook(full_df: pd.DataFrame) -> bytes:
         ws.write('G1', '', merge_format)
         ws.write('H1', '', merge_format)
 
-        # paint A3–A4, A27–A28, A51–A52, A75–A76 pink dynamically
-        for start in (2, 26, 50, 74):
-            for off in (1, 2):
-                ws.write(f'A{start + off}', '', format_date)
+        #PAINT Empty White Spaces
+        ws.write('A3', '', format_date)
+        ws.write('A4', '', format_date)
+        ws.write('A27', '', format_date)
+        ws.write('A28', '', format_date)
+        ws.write('A51', '', format_date)
+        ws.write('A52', '', format_date)
+        ws.write('A75', '', format_date)
+        ws.write('A76', '', format_date)
 
 
     workbook.close()
