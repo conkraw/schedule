1a68bb65b6191a3ab5eab137453cfa6d672dbc20
conkraw <conkraw@gmail.com>
2025-09-26 07:56:50 -0400

    Update pa.py

--- pa.py
+++ pa.py
@@ -27,7 +27,7 @@ from openpyxl.utils import column_index_from_string
 import math
 
 st.set_page_config(page_title="Batch Preceptor → REDCap Import", layout="wide")
-st.title("Batch Preceptor → REDCap Import Generator")
+st.title("PSUCOM PA SCHEDULE CREATOR")
 
 # ─── Sidebar mode selector ─────────────────────────────────────────────────────
 #mode = st.sidebar.radio("What do you want to do?",("Instructions", "Format OPD + Summary (4-sheet, 5-week)", "Create Student Schedule","OPD Check","Create Individual Schedules"))
